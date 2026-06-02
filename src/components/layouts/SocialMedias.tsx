d8d7d4424e2fd2e2d30926390d73067bd2f74e62
sanyamkohli99 <sanyamkohli99@gmail.com>
2026-06-02 18:37:17 +0530

    fix: build command, middleware location, hardcoded keys, branding

--- src/components/layouts/SocialMedias.tsx
+++ src/components/layouts/SocialMedias.tsx
@@ -11,7 +11,7 @@ type Props = {
 function SocialMedias({ containerClassName, itemsClassName }: Props) {
   return (
     <div className={cn("flex gap-x-5", containerClassName)}>
-      <Link href="https://github.com/clonglam/HIYORI-master" target="_blank">
+      <Link href="https://github.com/sanyamkohli99/Rozne" target="_blank">
         <Icons.gitHub
           className={cn(
             "w-4 h-4 md:w-5 md:h-5 text-muted-foreground hover:text-primary",
