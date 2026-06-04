088abe20a132e0c93ed7d6b064af0de02ad91b89
sanyamkohli99 <sanyamkohli99@gmail.com>
2026-06-04 16:11:09 +0530

    fix: re-export ProductImageShowcaseFragment from components index

--- src/features/products/components/index.ts
+++ src/features/products/components/index.ts
@@ -1,7 +1,7 @@
 export { default as BuyNowButton } from "./BuyNowButton";
 export { default as ProductCard, ProductCardFragment } from "./ProductCard";
 export { default as ProductCardSkeleton } from "./ProductCardSkeleton";
-export { default as ProductImageShowcase } from "./ProductImageShowcase";
+export { default as ProductImageShowcase, ProductImageShowcaseFragment } from "./ProductImageShowcase";
 export { default as RecommendationProducts } from "./RecommendationProducts";
 export { default as RecommendationProductsSkeleton } from "./RecommendationProductsSkeleton";
 export { default as SearchProductsGridSkeleton } from "../../search/components/SearchProductsGridSkeleton";
